659e3eebd53e9dccc49670b45bc60bd40d83accf
Zhijie Huang <t-zhhu@microsoft.com>
2019-12-16 18:02:58 +0800

    temp submit, creating sim space

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from problem_graph import create_sim_space
+from problem_graph import create_sim_space_826
 from wiring_rect import plotter_rect, svg2dwgscr_rect
 from wiring_bend import plotter_bend, svg2gds_bend
 # Demands
-File_Name = "./fiberBoard896.xls"
+# File_Name = "./fiberBoard896.xls"
+File_Name = "./fiberBoard826.xls"
 # the width of the fibers, unit: mm
 # Line_Width = 0.125
 Line_Width = 0.01
@@ -17,11 +18,11 @@ Bend_Radius = 5.0
 Delta_Arc = 0.001
 
 def main() -> None:
-    df = create_sim_space(File_Name, Save_Folder, Line_Width, Dist)
-    df_rect = plotter_rect(df, Line_Width, Dist+Line_Width, Save_Folder)
+    df = create_sim_space_826(File_Name, Save_Folder, Line_Width, Dist)
+    # df_rect = plotter_rect(df, Line_Width, Dist+Line_Width, Save_Folder)
     # svg2dwgscr_rect(df_rect, "fiberBoard896rect.scr", Save_Folder)
-    df_bend = plotter_bend(df_rect, Line_Width, Dist+Line_Width, Bend_Radius, Delta_Arc, Save_Folder)
-    svg2gds_bend(df_bend, Line_Width, Bend_Radius, "fiberBoard896bend.gds", Save_Folder)
+    # df_bend = plotter_bend(df_rect, Line_Width, Dist+Line_Width, Bend_Radius, Delta_Arc, Save_Folder)
+    # svg2gds_bend(df_bend, Line_Width, Bend_Radius, "fiberBoard896bend.gds", Save_Folder)
 
 
 if __name__ == "__main__":
--- problem_graph.py
+++ problem_graph.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+import sys
 
 def create_sim_space(file_name: str = "./fiberBoard896.xls", save_folder: str = './results/', line_width: float = 0.125, line_dist: float = 0.2) -> pd.DataFrame:
     data = pd.read_excel(file_name)
@@ -68,3 +69,57 @@ def create_sim_space(file_name: str = "./fiberBoard896.xls", save_folder: str = 
     # df.to_excel(save_folder +"fiberBoard896data.xlsx")
 
     return df
+
+
+def create_sim_space_826(file_name: str = "./fiberBoard826.xls", save_folder: str = './results/', line_width: float = 0.05, line_dist: float = 0.25) -> pd.DataFrame:
+    data = pd.read_excel(file_name)
+
+    BeginPointX = 0
+    BeginPointY = 0
+    BeginPointZ = 0
+
+    zoom_factor_x = 130.0 / 450
+    # zoom_factor_y = 100.0 / 70
+
+    above_list = [29, 26, 24, 20, 21, 18, 19, 16, 17,  2, 14, 15, 12, 13, 10, 11,  8,  9,  1,  6,  7]
+    above_dist = [48, 78, 50, 39, 21, 12,  8,  9,  8, 16, 19,  8, 10,  8, 14,  8, 25,  8,  8,  8,  8]
+    below_list = [59, 53, 54, 55, 56, 57, 58, 52, 47, 48, 49, 50, 51, 43, 44, 45, 46, 41, 42, 39, 38, 33, 32, 31, 30]
+    below_dist = [14,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 21,  8, 55, 25, 80, 19, 25, 25]
+
+    channels = [[0 for i in range(48)] for j in range(60)]
+
+    def find_1st_0(l):
+        return next((i for i, x in enumerate(l) if x == 0), None)
+
+    def layout_sn_ln(x):
+        try:
+            # find the 1st '0' in the list
+            p1 = find_1st_0(channels[x.Port1])
+            p2 = find_1st_0(channels[x.Port2])
+            while int(p1/12) != int(p2/12):
+                if int(p1/12) > int(p2/12):
+                    p2 = find_1st_0(channels[x.Port2][int(p1/12)*12:]) + int(p1/12)*12
+                elif int(p1/12) < int(p2/12):
+                    p1 = find_1st_0(channels[x.Port1][int(p2/12)*12:]) + int(p2/12)*12
+            channels[x.Port1][p1] = 1
+            channels[x.Port2][p2] = 1
+            return (p1, p2)
+        except:
+            print(x.Port1, x.Port2)
+            return (-1, -1)
+
+    def layout_sn(x):
+        sum(channels[x.Port1])
+
+    above_dist = np.cumsum(above_dist) * zoom_factor_x
+    below_dist = np.cumsum(below_dist) * zoom_factor_x
+
+    data["Port1"] = pd.to_numeric(data["Port1"].str.split(':', expand=True)[0].str[1:])
+    data["Port2"] = pd.to_numeric(data["Port2"].str.split(':', expand=True)[0].str[1:])
+    data["SN-LN"] = data.apply(lambda x: layout_sn_ln(x), axis=1)
+    # data["SN"] = data.apply(lambda x: layout_sn(x), axis=1)
+    # data["LN"] = data.apply(lambda x: layout_ln(x), axis=1)
+
+    data.to_excel(save_folder + "fiberBoard826data.xlsx")
+
+    return data
